c9ec270938a6804a44b0e249e5d1fc42feaab247
yeshashwork <yeshashwork@yahoo.com>
2022-10-15 16:45:10 -0400

    first commit

---
+++ wollowollo.java
@@ -0,0 +1,11 @@
+package wollowollo;
+
+public class wollowollo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("wollomejen");
+	System.out.println("fikir");
+	}
+
+}
